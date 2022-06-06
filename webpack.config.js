1da336f6661609a1dd382dabf128ab1047b71b9b
superfishal <97133716+superfishal@users.noreply.github.com>
2022-06-06 13:03:19 -0700

    manifest.json added - pwa working in browser local

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const webpack = require("webpack");
 module.exports = {
@@ -44,6 +45,23 @@ module.exports = {
     }),
     new BundleAnalyzerPlugin({
       analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [
+        {
+          src: path.resolve("assets/img/icons/icon-512x512.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
+      ],
     }),
   ],
   mode: "development",
